f9126a014a7c607a749ad8a8ae9097e20305c873
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-05-02 17:49:26 +0300

    Update enum.rs

--- enum.rs
+++ enum.rs
@@ -18,7 +18,7 @@ impl COMMAND {
 }
 
 pub fn print_command_value(value: COMMAND) {
-    println!("Val: {:#x}", value.get_val());
+    println!("CMD: {:?} | Val: {:#x}", value, value.get_val());
     return;
 }
 
@@ -27,6 +27,6 @@ fn main() {
     println!("CMD42: {:?}", COMMAND::COMMAND_42); // COMMAND_42
     println!("Val21: {:#x}", COMMAND::COMMAND_21.get_val()); // 0x21
     println!("Val42: {:#x}", COMMAND::COMMAND_42.get_val()); // 0x42
-    print_command_value(COMMAND::COMMAND_21); // 0x21
-    print_command_value(COMMAND::COMMAND_42); // 0x42
+    print_command_value(COMMAND::COMMAND_21); // COMMAND_21 | 0x21
+    print_command_value(COMMAND::COMMAND_42); // COMMAND_42 | 0x42
 }
