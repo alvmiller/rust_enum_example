f9433d5f17a5587784d3345efc198e7b803ecb51
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-05-04 11:47:10 +0300

    Update enum.rs

--- enum.rs
+++ enum.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 
-#[derive(Copy, Clone)]
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(u8)]
+//#[repr(packed)] // Error
 pub enum COMMAND {
 #[allow(non_camel_case_types)]
     COMMAND_21,
@@ -9,8 +10,17 @@ pub enum COMMAND {
     COMMAND_42,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(u8)]
+pub enum COMMAND1 {
+#[allow(non_camel_case_types)]
+    COMMAND_21(u8),
+#[allow(non_camel_case_types)]
+    COMMAND_42(u8),
+}
+
 impl COMMAND {
-    pub fn get_val(&self) -> u32 {
+    pub fn get_val(&self) -> u8 {
         match self {
             COMMAND::COMMAND_21 => 0x21,
             COMMAND::COMMAND_42 => 0x42
@@ -24,10 +34,15 @@ pub fn print_command_value(value: COMMAND) {
 }
 
 fn main() {
+    println!("{}", std::mem::size_of::<COMMAND>()); // 1
+    println!("{}", std::mem::size_of::<Option<COMMAND>>()); // 1
     println!("CMD21: {:?}", COMMAND::COMMAND_21); // COMMAND_21
     println!("CMD42: {:?}", COMMAND::COMMAND_42); // COMMAND_42
     println!("Val21: {:#x}", COMMAND::COMMAND_21.get_val()); // 0x21
     println!("Val42: {:#x}", COMMAND::COMMAND_42.get_val()); // 0x42
     print_command_value(COMMAND::COMMAND_21); // COMMAND_21 | 0x21
     print_command_value(COMMAND::COMMAND_42); // COMMAND_42 | 0x42
+    println!("-------------");
+    println!("{}", std::mem::size_of::<COMMAND1>()); // 2
+    println!("{}", std::mem::size_of::<Option<COMMAND1>>()); // 2
 }
